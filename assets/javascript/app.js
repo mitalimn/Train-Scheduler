0199cfa1b37a0db209ac246f8b9564211e7eaa50
mitalimn <mitalinaik@yahoo.com>
2016-12-05 10:31:23 -0600

    firebase database created

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC585hLdaNPpXSIlKEsGLQtWdl1geE0DvY",
+    authDomain: "train-scheduler-10459.firebaseapp.com",
+    databaseURL: "https://train-scheduler-10459.firebaseio.com",
+    storageBucket: "train-scheduler-10459.appspot.com",
+    messagingSenderId: "79763209333"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
